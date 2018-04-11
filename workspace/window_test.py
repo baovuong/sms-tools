06113804b17a3654afba167f98aba8727cddda5d
Bao Vuong <vqbao@lv5.org>
2018-04-11 14:28:30 -0400

    fixed up window_test

--- workspace/window_test.py
+++ workspace/window_test.py
@@ -16,7 +16,7 @@ hM1 = int(math.floor((M+1)/2))
 hM2 = int(math.floor(M/2))
 
 N = 512
-hN = N/2 + 1
+hN = int(N/2)
 fftbuffer = np.zeros(N)
 fftbuffer[:hM1] = window[hM2:]
 fftbuffer[N-hM2:] = window[:hM2]
