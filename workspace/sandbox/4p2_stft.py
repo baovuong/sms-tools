ea1abcf17f07f4218fccb00d354206c99cddf3e6
Bao Vuong <vqbao@lv5.org>
2018-05-01 17:02:05 -0400

    added code

--- workspace/sandbox/4p2_stft.py
+++ workspace/sandbox/4p2_stft.py
@@ -1,3 +1,24 @@
-
 # Short-time Fourier Transform
 
+import numpy as np
+import matplotlib.pyplot as plt
+import os, sys 
+from scipy.signal import get_window 
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(sys.path[0])), 'software', 'models'))
+import utilFunctions as UF
+import stft as STFT 
+
+inputFile = '..\\..\\sounds\\flute-A4.wav'
+window = 'hamming'
+M = 801 
+N = 1024 
+H = 400
+
+fs, x = UF.wavread(inputFile)
+
+w = get_window(window, M)
+
+mX, pX = STFT.stftAnal(x, fs, w, N, H)
+
+plt.plot(x)
+plt.show()
